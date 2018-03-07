2abf1fad779cd810919eff911836864adbe6447b
Dante Cerron <dacerron@gmail.com>
2018-03-06 19:44:08 -0800

    bare bones

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 
+const location = path.resolve(__dirname);
 const hostname = '127.0.0.1'
 const port = 3000
+const htmlpath = "/html"
 
-app.get('/', (req, res) => res.send('Hello world!'))
 
-app.get('/admin', (req, res) => res.send('You should only be here if you are an admin'))
+app.get('/admin', (req, res) => res.sendFile(location + htmlpath + "/admin.html"))
 
-app.get('/customer', (req,res) => res.send('Hello valued customer!'))
+app.get('/customer', (req,res) => res.sendFile(location + htmlpath + "/customer.html"))
 
-app.get('/company', (req,res) => res.send('Welcome to your company page'))
+app.get('/company', (req,res) => res.sendFile(location + htmlpath + "/company.html"))
+
+app.get('/', (req, res) => res.sendFile(location + htmlpath + "/index.html"))
 
 app.listen(port, () => {
   console.log('Example app listening on port ' + port)
